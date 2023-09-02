b10a90ff887ae70b8f3ac3299f5c7fda55ce8ee2
monjara <yaginumee@gmail.com>
2023-09-02 14:39:40 +0900

    sample warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    warp::serve(hello).run(([0, 0, 0, 0], 8080)).await;
 }
